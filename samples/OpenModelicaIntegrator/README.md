ab8c5e65296388836df55d7c76152d5e7dd0bda2
Forrest Trepte <forrest@microsoft.com>
2021-04-15 11:50:52 -0700

    copy from vanDerPol sample

---
+++ samples/OpenModelicaIntegrator/README.md
@@ -0,0 +1,135 @@
+# FMU Example: Van der Pol
+
+This simulator simulates a Van der Pol frequency response. It is not a steady-state simulator.
+The brain will learn to adjust an input parameter to control the oscillator.
+
+The simulator is implemented by [vanDerPol.fmu](https://github.com/modelica/fmi-cross-check/blob/master/fmus/2.0/cs/win64/FMUSDK/2.0.4/vanDerPol/vanDerPol.fmu) from the FMI Cross-Check repository.
+
+## States
+
+| State         | Range            | Notes    |
+| ------------- | ---------------- | -------- |
+| x0            | [-Inf ... Inf]   | Main direction of oscillation of a Van der Pol system. |
+| x1            | [-Inf ... Inf]   | Second direction of oscillation of a Van der Pol system. |
+| derx0         | [-Inf ... Inf]   | Speed on first direction. |
+| derx1         | [-Inf ... Inf]   | Speed on second direction. |
+
+Note, the main behavior of a Van der Pol system is that x1, will oscillate with same magnitude than the speed in x0 (derx0).
+
+## Actions
+
+| State               | Range                | Notes    |
+| ------------------- | -------------------- | -------- |
+| x0_adjust           | [-0.2 ... 0.2]       | Adjustment on first direction of Van der Pol system. |
+
+In this Bonsai theoretical approach to the Van der Pol problem, it is assumed that we can perform small perturbations in only one of the directions (in x0).
+The goal of the brain will be minimizing the oscillation as much as possible, adjusting to variable configuration parameters (see section bellow).
+
+## Configuration Parameters
+
+| State               | Range                | Notes    |
+| ------------------- | -------------------- | -------- |
+| mu                  | [0.5 ... 4]          | Oscillation parameter. |
+
+This parameter will change the response of x1 to x0. Changing this parameter during training should further ensure we generalize to different scenarios (avoiding overfitting).
+
+## Setting up: Installation & Bonsai configuration
+
+Go to this project's [README.md](../../README.md) to review:
+
+- INSTALLATIONS REQUIRED (conda & setting environment up)
+- SETTING UP BONSAI CONFIGURATION
+
+***Note, this example only runs in win64***
+
+## Running the model: Local simulator
+
+Open an Anaconda Prompt window.
+
+1. Activate Anaconda environment:
+
+        conda activate fmu_env
+
+2. Point to the "samples" folder and get inside any of the proposed examples
+
+3. Create a new brain and push INK file:
+
+        bonsai brain create -n vanDerPol_v0
+        bonsai brain version update-inkling -f machine_teacher.ink -n vanDerPol_v0
+
+4. Start simulator using:
+
+        python main.py
+
+    - Note, for new examples, you will have to verify the set of {inputs/outputs/config params} is correct.
+    - Check the CMD Prompt to follow instructions.
+    - The moment you answer 'y' at least once, the config will be validated.
+    - You can disable being prompted again by setting FIRST_TIME_RUNNING to False in main.py:
+
+    > FIRST_TIME_RUNNING = False
+
+5. Open a new Anaconda Prompt and activate the environment too
+
+        conda activate fmu_env
+
+6. Start brain training from CLI
+
+        bonsai brain version start-training -n vanDerPol_v0
+
+7. Connect simulators to unmanaged local sim:
+
+        bonsai simulator unmanaged connect -b vanDerPol_v0 -a Train -c ReduceOscillation --simulator-name VanDerPol_Oscillations
+
+If the simulation is running successfully, command line output should print "Registered simulator".
+The Bonsai workspace should show the FMU simulator name under the Simulators section, listed as Unmanaged.
+
+## Running the model: Scaling your simulator
+
+Once you have confirmed input/outputs of the model through command prompt, you can go ahead and disable authentication.
+Open [main.py](main.py) and set FIRST_TIME_RUNNING to False (remember to set this to False, when transferring this examples to new models):
+
+> FIRST_TIME_RUNNING = False
+
+- This step ensures the image is not waiting for user input to start the simulation. Config file approved by user is used.
+once variable is set to False. In our case the sim configuration should be located at:
+[sim/vanDerPol.yaml](sim/vanDerPol_conf.yaml)
+
+Then, on an Anaconda Prompt window
+
+1. Go to the "samples\vanDerPol" folder
+
+2. Create a new brain and push INK file:
+
+        bonsai brain create -n vanDerPol_v0
+        bonsai brain version update-inkling -f machine_teacher.ink -n vanDerPol_v0
+
+3. Go back to FMU-bonsai-connector root
+4. Log in into Azure and push image
+
+       az login
+       az acr build --image fmu_image_vanderpol:1 --platform windows --file Dockerfile-windows_vanDerPol --registry <ACR_REGISTRY_NAME> .
+
+    *Note, ACR Registry can be extracted from preview.bons.ai --> Workspace ACR path == <ACR_REGISTRY_NAME>.azurecr.io
+
+    *Also, feel free to rerun before troubleshooting if you find the following error:
+        
+    > "container XXX encountered an error during hcsshim::System::Start: context deadline exceeded
+    >
+    > 2021/03/20 02:10:06 Container failed during run: build. No retries remaining.
+    >
+    > failed to run step ID: build: exit status 1
+
+5. Go to preview.bons.ai
+6. Click over "Add Sim" > "Other", and insert the location of the image:
+
+    - Azure Container Registry image path:  <ACR_REGISTRY_NAME>.azurecr.io/fmu_image_vanDerPol:1
+
+    - Simulator package display name:  fmu_image_vanderpol_v1
+
+7. Add package name to INK file:
+
+    - Modify "source simulator Simulator([...]) \{ }" into "source simulator Simulator([...]) {_"fmu_image_vanderpol_v1"_}"
+
+** Check [adding a training simulator to your Bonsai workspace](https://docs.microsoft.com/en-us/bonsai/guides/add-simulator?tabs=add-cli%2Ctrain-inkling&pivots=sim-platform-other)
+for further information about how to upload the container, add it to Bonsai, and scale the simulator.
+
---
+++ samples/OpenModelicaIntegrator/main.py
@@ -0,0 +1,397 @@
+#!/usr/bin/env python
+"""
+Microsoft-Bonsai-API integration with House Energy Simulator
+"""
+
+# [TODO] Review package import... not clean at the moment
+import os
+import sys
+dir_path = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, dir_path + "//..//..//FMU_Connector")
+
+import pathlib
+import time
+import datetime
+from distutils.util import strtobool
+from typing import Any, Dict, List, Union
+
+from dotenv import load_dotenv, set_key
+from FMU_Connector import FMUConnector
+from microsoft_bonsai_api.simulator.client import BonsaiClient, BonsaiClientConfig
+from microsoft_bonsai_api.simulator.generated.models import (
+    SimulatorInterface,
+    SimulatorState,
+)
+
+
+from policies import random_policy
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+log_path = "logs"
+
+# TODO_PER_SIM 10: Turn to False after model in/out config vars have been verified
+# - You can turn this off once you are satisfied with the config saved to "sim\{model_name}_conf.yaml"
+FIRST_TIME_RUNNING = True
+
+# TODO_PER_SIM 1: read FMI version from modelDescription.xml
+# - you can manually unzip the folder to check, or run with FMI_VERSIOn=2.0, and get it unpacked
+# ("1.0", "2.0", "3.0")
+FMI_VERSION = "2.0"
+
+# TODO_PER_SIM 2: Set to True when dealing with a steady state simulator (no time evolution involved)
+STEADY_STATE_SIM = False
+
+# TODO_PER_SIM 3: define start, stop, and step size
+# - stop and step size values are overwritten for steady state sims
+START_TIME = 0.0
+STOP_TIME = 20.0
+STEP_SIZE = 0.1
+# if steady-state sim, default to {STOP_TIME = 1.0} & {STEP_SIZE = 0.1}
+if STEADY_STATE_SIM:
+    STOP_TIME = 1.0
+    STEP_SIZE = 0.1
+
+# TODO_PER_SIM 4: define default config file (if None provided by brain)
+DEFAULT_CONFIG = {"mu": 1.5,}
+
+class FMUSimulatorSession:
+    # TODO_PER_SIM 5: Set-up model filepath (modeldir) & sim name (env_name) variables
+    def __init__(
+        self,
+        modeldir: str = "sim\\vanDerPol.fmu",
+        env_name: str = "VanDerPol_Oscillations",
+        log_file: Union[str, None] = None,
+    ):
+        """Template for simulating FMU models with FMUConnector
+
+        Parameters
+        ----------
+        modeldir: str, optional
+            relative filepath to your FMU sim (e.g: "sim\\vanDerPol.fmu", if in sim subfolder)
+        env_name: str, optional
+            name of simulator environment, registered by SimulatorInterface
+            note, this will be your sim name in preview.bons.ai
+        """
+
+        self.modeldir = modeldir
+        self.model_full_path = os.path.join(dir_path, self.modeldir)
+        self.env_name = env_name
+        print("Using simulator file from: ", self.model_full_path)
+
+        self.default_config = DEFAULT_CONFIG
+
+        # Validate and instance FMU model
+        self.simulator = FMUConnector(model_filepath = self.model_full_path,
+                                      fmi_version = FMI_VERSION,
+                                      start_time = START_TIME,
+                                      stop_time = STOP_TIME,
+                                      step_size = STEP_SIZE,
+                                      user_validation = FIRST_TIME_RUNNING)
+
+        # initialize model - required!
+        self.simulator.initialize_model()
+
+        self._reset()
+        self.terminal = False
+        if not log_file:
+            current_time = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+            log_file = current_time + "_" + env_name + "_log.csv"
+            log_file = os.path.join(log_path, log_file)
+            logs_directory = pathlib.Path(log_file).parent.absolute()
+            if not pathlib.Path(logs_directory).exists():
+                print(
+                    "Directory does not exist at {0}, creating now...".format(
+                        str(logs_directory)
+                    )
+                )
+                logs_directory.mkdir(parents=True, exist_ok=True)
+        self.log_file = os.path.join(log_path, log_file)
+
+    def get_state(self) -> Dict[str, float]:
+        """ Called to retreive the current state of the simulator. """
+
+        # TODO_PER_SIM 6: (optional) Modify states to be sent to Bonsai
+        # - currently, all states are sent to Bonsai: config_params, states (sim outputs), and actions (sim inputs)
+        return self.simulator.get_all_vars()
+
+
+    def _reset(self, config: dict = None):
+        """Helper function for resetting a simulator environment
+           Runs with config provided by Bonsai (if given), uses default config otherwise
+
+        Parameters
+        ----------
+        config : dict, optional
+            [description], by default None
+        """
+
+        if config:
+            if len(config.items()) > 0:
+                self.sim_config = config
+            else:
+                self.sim_config = self.default_config
+        else:
+            self.sim_config = self.default_config
+
+        self.simulator.reset(self.sim_config)
+
+
+    def episode_start(self, config: Dict[str, Any] = None):
+        """Method invoked at the start of each episode to reset the sim with a given episode configuration.
+
+        Parameters
+        ----------
+        config : Dict[str, Any]
+            SimConfig parameters for the current episode defined in Inkling
+        """
+
+        self._reset(config)
+
+
+    def episode_step(self, action: Dict[str, Any]):
+        """Called for each step of the episode 
+
+        Parameters
+        ----------
+        action : Dict[str, Any]
+            BrainAction chosen from the Bonsai Service, prediction or exploration
+        """
+        
+        # TODO_PER_SIM 7: Add any action transformation required (from Bonsai to sim)
+        # Apply actions
+        # Transform state to apply differential
+        # --> 'x0' += 'x0_adjust'
+        x0_adjust = action['x0_adjust']
+        sim_action_val = self.simulator.get_states(['x0'])['x0']
+        sim_action = {'x0': sim_action_val + x0_adjust}
+        self.simulator.apply_actions(sim_action)
+
+        # Run sim one step forward
+        # - Stepping is performed only on sims that evolve over time (STEADY_STATE_SIM == False)
+        if not STEADY_STATE_SIM:
+            self.simulator.run_step()
+
+
+    def halted(self) -> bool:
+        """Should return True if the simulator cannot continue"""
+        # TODO_PER_SIM 8: Define any halt conditions
+        return self.terminal
+
+    def random_policy(self, state: Dict = None) -> Dict:
+        # TODO_PER_SIM 9: Update the random policy to be used for the example on policies.py
+        return random_policy()
+
+    def log_iterations(self, state, action, episode: int = 0, iteration: int = 1):
+        """Log iterations during training to a CSV.
+
+        Parameters
+        ----------
+        state : Dict
+        action : Dict
+        episode : int, optional
+        iteration : int, optional
+        """
+
+        import pandas as pd
+
+        def add_prefixes(d, prefix: str):
+            return {f"{prefix}_{k}": v for k, v in d.items()}
+
+        state = add_prefixes(state, "state")
+        action = add_prefixes(action, "action")
+        config = add_prefixes(self.default_config, "config")
+        data = {**state, **action, **config}
+        data["episode"] = episode
+        data["iteration"] = iteration
+        log_df = pd.DataFrame(data, index=[0])
+
+        if os.path.exists(self.log_file):
+            log_df.to_csv(
+                path_or_buf=self.log_file, mode="a", header=False, index=False
+            )
+        else:
+            log_df.to_csv(path_or_buf=self.log_file, mode="w", header=True, index=False)
+
+
+def env_setup():
+    """Helper function to setup connection with Project Bonsai
+
+    Returns
+    -------
+    Tuple
+        workspace, and access_key
+    """
+
+    load_dotenv(verbose=True)
+    workspace = os.getenv("SIM_WORKSPACE")
+    access_key = os.getenv("SIM_ACCESS_KEY")
+
+    env_file_exists = os.path.exists(".env")
+    if not env_file_exists:
+        open(".env", "a").close()
+
+    if not all([env_file_exists, workspace]):
+        workspace = input("Please enter your workspace id: ")
+        set_key(".env", "SIM_WORKSPACE", workspace)
+    if not all([env_file_exists, access_key]):
+        access_key = input("Please enter your access key: ")
+        set_key(".env", "SIM_ACCESS_KEY", access_key)
+
+    load_dotenv(verbose=True, override=True)
+    workspace = os.getenv("SIM_WORKSPACE")
+    access_key = os.getenv("SIM_ACCESS_KEY")
+
+    return workspace, access_key
+
+
+def test_random_policy(
+    num_episodes: int = 10,
+    log_iterations: bool = False,
+    max_iterations: int = 288,
+):
+    """Test a policy using random actions over a fixed number of episodes
+
+    Parameters
+    ----------
+    num_episodes : int, optional
+        number of iterations to run, by default 10
+    """
+
+    sim = FMUSimulatorSession(log_file="VanDerPol_Oscillations.csv") 
+    for episode in range(num_episodes):
+        iteration = 0
+        terminal = False
+        obs = sim.episode_start()
+        while not terminal:
+            action = sim.random_policy()
+            sim.episode_step(action)
+            sim_state = sim.get_state()
+            if log_iterations:
+                sim.log_iterations(
+                    state=sim_state, action=action, episode=episode, iteration=iteration
+                )
+            print(f"Running iteration #{iteration} for episode #{episode}")
+            print(f"Observations: {sim_state}")
+            iteration += 1
+            terminal = iteration > max_iterations
+
+
+def main(config_setup: bool = False):
+    """Main entrypoint for running simulator connections
+
+    Parameters
+    ----------
+    config_setup : bool, optional
+        apply config setup using .env file, by default False
+    """
+
+    # workspace environment variables
+    if config_setup:
+        env_setup()
+        load_dotenv(verbose=True, override=True)
+
+    # Grab standardized way to interact with sim API
+    sim = FMUSimulatorSession()
+
+    # Configure client to interact with Bonsai service
+    config_client = BonsaiClientConfig()
+    client = BonsaiClient(config_client)
+
+    # # Load json file as simulator integration config type file
+    # with open("interface.json") as file:
+    #     interface = json.load(file)
+
+    # Create simulator session and init sequence id
+    registration_info = SimulatorInterface(
+        name=sim.env_name,
+        timeout=60,
+        simulator_context=config_client.simulator_context,
+    )
+    registered_session = client.session.create(
+        workspace_name=config_client.workspace, body=registration_info
+    )
+    print("Registered simulator.")
+    sequence_id = 1
+
+    try:
+        while True:
+            # Advance by the new state depending on the event type
+            sim_state = SimulatorState(
+                sequence_id=sequence_id, state=sim.get_state(), halted=sim.halted(),
+            )
+            event = client.session.advance(
+                workspace_name=config_client.workspace,
+                session_id=registered_session.session_id,
+                body=sim_state,
+            )
+            sequence_id = event.sequence_id
+            print(f'[{time.strftime("%H:%M:%S")}] Last Event: {event.type}, Sim Time: {sim.simulator.sim_time:.3f}')
+
+            # Event loop
+            if event.type == "Idle":
+                time.sleep(event.idle.callback_time)
+                print("Idling...")
+            elif event.type == "EpisodeStart":
+                sim.episode_start(event.episode_start.config)
+            elif event.type == "EpisodeStep":
+                sim.episode_step(event.episode_step.action)
+            elif event.type == "EpisodeFinish":
+                print("Episode Finishing...")
+            elif event.type == "Unregister":
+                print("Simulator Session unregistered by platform because '{}', Registering again!".format(event.unregister.details))
+                client.session.delete(
+                    workspace_name=config_client.workspace,
+                    session_id=registered_session.session_id,
+                )
+                print("Unregistered simulator.")
+            else:
+                pass
+    except KeyboardInterrupt:
+        # Gracefully unregister with keyboard interrupt
+        client.session.delete(
+            workspace_name=config_client.workspace,
+            session_id=registered_session.session_id,
+        )
+        print("Unregistered simulator.")
+    except Exception as err:
+        # Gracefully unregister for any other exceptions
+        client.session.delete(
+            workspace_name=config_client.workspace,
+            session_id=registered_session.session_id,
+        )
+        print("Unregistered simulator because: {}".format(err))
+
+
+if __name__ == "__main__":
+
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Bonsai and Simulator Integration...")
+    parser.add_argument(
+        "--log-iterations",
+        type=lambda x: bool(strtobool(x)),
+        default=False,
+        help="Log iterations during training",
+    )
+    parser.add_argument(
+        "--config-setup",
+        type=lambda x: bool(strtobool(x)),
+        default=True,
+        help="Use a local environment file to setup access keys and workspace ids",
+    )
+    parser.add_argument(
+        "--test-local",
+        type=lambda x: bool(strtobool(x)),
+        default=False,
+        help="Run simulator locally without connecting to platform",
+    )
+
+    args = parser.parse_args()
+
+    if args.test_local:
+        test_random_policy(
+            num_episodes=1000, log_iterations=args.log_iterations
+        )
+    else:
+        main(config_setup=args.config_setup)
+
